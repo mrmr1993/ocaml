7d45f54b49ae2d2a5101b3a5d103b0aec892649c
mrmr1993 <mr_1993@hotmail.co.uk>
2020-06-14 09:47:35 +0100

    Add id_pairs to normalize_type

--- typing/ctype.ml
+++ typing/ctype.ml
@@ -4765,7 +4765,7 @@ let closed_schema env ty =
 
 (* Normalize a type before printing, saving... *)
 (* Cannot use mark_type because deep_occur uses it too *)
-let rec normalize_type_rec visited ty =
+let rec normalize_type_rec id_pairs visited ty =
   let ty = repr ty in
   if not (TypeSet.mem ty !visited) then begin
     visited := TypeSet.add ty !visited;
@@ -4787,7 +4787,9 @@ let rec normalize_type_rec visited ty =
                 List.fold_left
                   (fun tyl ty ->
                     if List.exists
-                        (fun ty' -> equal Env.empty false [] [] [ty] [ty']) tyl
+                        (fun ty' ->
+                          equal Env.empty false id_pairs id_pairs [ty] [ty'])
+                        tyl
                     then tyl else ty::tyl)
                   [ty] tyl
               in
@@ -4825,11 +4827,11 @@ let rec normalize_type_rec visited ty =
         set_type_desc fi fi'.desc
     | _ -> ()
     end;
-    iter_type_expr (normalize_type_rec visited) ty
+    iter_type_expr (normalize_type_rec id_pairs visited) ty
   end
 
 let normalize_type ty =
-  normalize_type_rec (ref TypeSet.empty) ty
+  normalize_type_rec [] (ref TypeSet.empty) ty
 
 
                               (*************************)
