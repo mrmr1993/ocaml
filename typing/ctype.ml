7723dc4d4ca560fcd079b1e832db7b0abcc91818
mrmr1993 <mr_1993@hotmail.co.uk>
2020-06-14 05:17:05 +0100

    Remove id_pairs argument to reify, add a TODO for unscoped idents

--- typing/ctype.ml
+++ typing/ctype.ml
@@ -2165,7 +2165,7 @@ let get_gadt_equations_level () =
 (* a local constraint can be added only if the rhs
    of the constraint does not contain any Tvars.
    They need to be removed using this function *)
-let reify env id_pairs t =
+let reify env t =
   let fresh_constr_scope = get_gadt_equations_level () in
   let create_fresh_constr lev name =
     let name = match name with Some s -> "$'"^s | _ -> "$" in
@@ -2210,11 +2210,12 @@ let reify env id_pairs t =
           iter_row (iterator env id_pairs) r
       | Tconstr (p, _, _) when is_object_type p ->
           iter_type_expr (iterator env id_pairs) (full_expand !env id_pairs ty)
+      (* TODO: Throw an error here if a type refers to an unscoped identifier. *)
       | _ ->
           iter_type_expr (iterator env id_pairs) ty
     end
   in
-  iterator env id_pairs t
+  iterator env [] t
 
 let is_newtype env p =
   try
@@ -2780,7 +2781,7 @@ and unify3 env t1 t1' t2 t2' =
                     let snap = snapshot () in
                     try unify env t1 t2 with Unify _ ->
                       backtrack snap;
-                      reify env [] t1; reify env [] t2
+                      reify env t1; reify env t2
                   end)
               inj (List.combine tl1 tl2)
       | (Tconstr (path,[],_),
@@ -2796,17 +2797,17 @@ and unify3 env t1 t1' t2 t2' =
           add_gadt_equation env source destination
       | (Tconstr (path,[],_), _)
         when is_instantiable !env path && can_generate_equations () ->
-          reify env [] t2';
+          reify env t2';
           record_equation t1' t2';
           add_gadt_equation env path t2'
       | (_, Tconstr (path,[],_))
         when is_instantiable !env path && can_generate_equations () ->
-          reify env [] t1';
+          reify env t1';
           record_equation t1' t2';
           add_gadt_equation env path t1'
       | (Tconstr (_,_,_), _) | (_, Tconstr (_,_,_)) when !umode = Pattern ->
-          reify env [] t1';
-          reify env [] t2';
+          reify env t1';
+          reify env t2';
           if can_generate_equations () then (
             mcomp !env t1' t2';
             record_equation t1' t2'
@@ -2830,8 +2831,8 @@ and unify3 env t1 t1' t2 t2' =
             try unify_row env row1 row2
             with Unify _ ->
               backtrack snap;
-              reify env [] t1';
-              reify env [] t2';
+              reify env t1';
+              reify env t2';
               if can_generate_equations () then (
                 mcomp !env t1' t2';
                 record_equation t1' t2'
@@ -2863,7 +2864,7 @@ and unify3 env t1 t1' t2 t2' =
               t1.level p1 n1 tl1 t2.level p2 n2 tl2
           with Not_found ->
             if !umode = Expression then raise (Unify []);
-            List.iter (reify env []) (tl1 @ tl2);
+            List.iter (reify env) (tl1 @ tl2);
             (* if !generate_equations then List.iter2 (mcomp !env) tl1 tl2 *)
           end
       | (Tnil,  Tconstr _ ) -> raise (Unify Trace.[Obj(Abstract_row Second)])
